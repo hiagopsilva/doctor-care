d059ecca82a800abcb9f2b21ed9f2697bd3a13f3
Hiago Prates <hiagohps360@gmail.com>
2022-08-19 03:51:54 -0300

    chore: added assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,3 +5,4 @@ export { default as Doctor } from './doctor.png';
 export { default as Men } from './men.png';
 export { default as Mail } from './Mail';
 export { default as Maps } from './Maps';
+export { default as Whatsapp } from './Whatsapp';
