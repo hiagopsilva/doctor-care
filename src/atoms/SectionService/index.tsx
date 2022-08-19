cafa46656c2ac6779d2f6a5a78ddfa41d997bc8f
Hiago Prates <hiagohps360@gmail.com>
2022-08-19 12:23:21 -0300

    fix: responsiveness

--- src/atoms/SectionService/index.tsx
+++ src/atoms/SectionService/index.tsx
@@ -38,11 +38,8 @@ const SectionService: FC<Props> = () => {
             <Icon />
             <Text>{item}</Text>
             <Description>
-              Amet minim mollit non deserunt
-              <br />
-              ullamco est sit aliqua dolor do amet sint.
-              <br />
-              Velit officia consequat duis enim.
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim.
             </Description>
           </Card>
         ))}
