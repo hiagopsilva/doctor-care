a84ef95f308d396304581fbafac6825f829becc5
Hiago Prates <hiagohps360@gmail.com>
2022-08-19 03:11:29 -0300

    chore: added assets doctor

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
 export { default as Logo } from './Logo';
 export { default as Women } from './Women';
 export { default as Check } from './Check';
+export { default as Doctor } from './doctor.png';
