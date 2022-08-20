efba6884d7087008e163a9963e3ac0aa355dd62b
Hiago Prates <hiagohps360@gmail.com>
2022-08-19 22:56:03 -0300

    chore: update

--- src/atoms/SectionAbout/styles.ts
+++ src/atoms/SectionAbout/styles.ts
@@ -8,18 +8,32 @@ const primaryMain = getTheme('primary.main');
 const primaryDark = getTheme('primary.dark');
 const secondaryDark = getTheme('secondary.dark');
 
+const inMobile = getTheme('inMobile');
+
 export const Container = styled.div`
   height: ${pxToRem(734)};
   width: 100%;
 
   background-color: ${tertiaryLight};
   padding: 0 ${pxToRem(124)};
+
+  @media ${inMobile} {
+    height: auto;
+    padding: 0 ${pxToRem(29)};
+  }
 `;
 
 export const Wrapper = styled.div`
   margin-top: ${pxToRem(159)};
 
   display: flex;
+
+  @media ${inMobile} {
+    height: auto;
+    margin-top: ${pxToRem(80)};
+
+    flex-direction: column-reverse;
+  }
 `;
 
 export const Image = styled.img`
@@ -33,6 +47,13 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+
+  @media ${inMobile} {
+    margin-left: ${pxToRem(29)};
+
+    width: 100%;
+    background-color: red;
+  }
 `;
 
 export const Text = styled.div`
